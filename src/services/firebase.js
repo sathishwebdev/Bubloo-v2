ee2484a38d5f2ea72685324d9bac1cb9bef4d8f6
Sathish Kumar <sathishweb27@gmail.com>
2022-03-09 20:43:23 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,21 +4,17 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD6J7rCHRp1As1mInXX9Ct52C2zAYxiu6o",
-    authDomain: "bubloo2716.firebaseapp.com",
-    databaseURL: "https://bubloo2716-default-rtdb.firebaseio.com",
-    projectId: "bubloo2716",
-    storageBucket: "bubloo2716.appspot.com",
-    messagingSenderId: "1025422111699",
-    appId: "1:1025422111699:web:e6dbcbf08a85f76321f526",
-    measurementId: "G-GJ8TQB70RK"
+    apiKey: process.env.REACT_APP_API_KEY ,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
   const fire = firebase.initializeApp(firebaseConfig);
 
   export default fire
   export const auth = fire.auth();
   export const db = fire.firestore();
-
-
-// WEBPACK FOOTER //
-// ./src/services/firebase.js
